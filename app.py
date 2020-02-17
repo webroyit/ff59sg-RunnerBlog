ce8731204374f21de9f28d1663da5f01ab4f47a4
royweb <xytank@gmail.com>
2020-02-17 13:49:42 -0500

    Add more route

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 # double underscore for the name of the module
 app = Flask(__name__)
 
+# you can add more endpoints to use the same function
 @app.route("/")
+@app.route("/home")
 def hello():
     return "<h1>It Works</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 if __name__ == "__main__":
     # restart the server
