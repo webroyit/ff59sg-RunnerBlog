62a7e72ccdbc3a7a246de4f9bbedf63e53cb0cf3
royweb <xytank@gmail.com>
2020-02-17 15:58:59 -0500

    Display some dummy data on the home page

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template
 # double underscore for the name of the module
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Bob Test",
+        "title": "First Time Running",
+        "content": "It was fun to run around",
+        "date_posted": "Feb 15, 2020"
+    },
+    {
+        "author": "Rob Run",
+        "title": "Running At the Park",
+        "content": "I enjoyed running at the park",
+        "date_posted": "Feb 17, 2020"
+    }
+]
+
 # you can add more endpoints to use the same function
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts = posts)
 
 @app.route("/about")
 def about():
