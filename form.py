8dc910f4a47e13089a965ffd23c33d57725b5ff4
royweb <xytank@gmail.com>
2020-02-19 12:42:23 -0500

    Set up registration form

---
+++ form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtfomrs.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    # DataRequired() = input cannot be empty
+    # Length() =  length of the input
+    # Email() = check if the input is an email
+    # EqualTo() = check if the password and confirm password match
+    username = StringField("Username", validators = [DataRequired(), Length(min = 3, max = 20)])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
+    confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password"), Length(min = 6, max = 20)])
+    submit = SubmitField("Create Account")
