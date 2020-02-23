e5f1a610ed22468f2621d254db4595bcaa0968a5
royweb <xytank@gmail.com>
2020-02-23 12:59:53 -0500

    Add a relationship between Post and User

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ class User(db.Model):
     image_file = db.Column(db.String(20), nullable = False, default = "default.jpg")
     password = db.Column(db.String(60), nullable = False)
 
+    # adding one to many relationship
+    # backref create a new column
+    # lazy to load the data from the database when it is needed
+    posts = db.relationship("Post", backref = "author", lazy = True)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
@@ -29,6 +34,9 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable = False)
     date_posted = db.Column(db.DateTime, nullable = False,  default = datetime.utcnow)      # no () after utcnow otherwise the time will set when the server start
     content = db.Column(db.Text, nullable = False)
+
+    # get the user id from the User class
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
