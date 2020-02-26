3a30458bda3761b3dac64926bc5944e142c8eb42
royweb <xytank@gmail.com>
2020-02-26 12:45:32 -0500

    Update login route to create session for the user

--- runnerblog/routes.py
+++ runnerblog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from runnerblog import app, db, bcrypt
 from runnerblog.forms import RegistrationForm, LoginForm
 from runnerblog.models import User, Post
+from flask_login import login_user
 
 posts = [
     {
@@ -56,9 +57,11 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        # for testing
-        if form.email.data == "test@testing.com" and form.password.data == "123456":
-            flash(f"Welcome back!", "success")
+        user = User.query.filter_by(email = form.email.data).first()
+
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            # login the user
+            login_user(user, remember = form.remember.data)
             return redirect(url_for("home"))
         else:
             flash(f"Login failed, please try again", "danger")
