11c153f77aa3c6ec194ee21c8e3c492c339dd32a
royweb <xytank@gmail.com>
2020-02-26 11:31:24 -0500

    Add custom validation

--- runnerblog/forms.py
+++ runnerblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from runnerblog.models import User
 
 class RegistrationForm(FlaskForm):
     # DataRequired() = input cannot be empty
@@ -12,6 +13,22 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
     confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password"), Length(min = 6, max = 20)])
     submit = SubmitField("Create Account")
+
+    # custom validation
+    def validate_username(self, username):
+        # find the user in the database
+        user = User.query.filter_by(username = username.data).first()
+
+        if user:
+            # send a message
+            raise ValidationError("Username aleady existed")
+
+    def validate_email(self, email):
+        # find the email in the database
+        user = User.query.filter_by(email = email.data).first()
+
+        if user:
+            raise ValidationError("Email aleady existed")
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators = [DataRequired(), Email()])
