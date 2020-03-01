521b2a307a938d4a26605c566d95030edeff6fab
royweb <xytank@gmail.com>
2020-03-01 12:48:44 -0500

    Set up update account form

--- runnerblog/forms.py
+++ runnerblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from runnerblog.models import User
@@ -35,3 +36,22 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
     remember = BooleanField("Remember me")
     submit = SubmitField("Login")
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators = [DataRequired(), Length(min = 3, max = 20)])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+
+            if user:
+                raise ValidationError("Username aleady existed")
+
+    def validate_email(self, email):
+        if username.email != current_user.username:
+            user = User.query.filter_by(email = email.data).first()
+
+            if user:
+                raise ValidationError("Email aleady existed")
