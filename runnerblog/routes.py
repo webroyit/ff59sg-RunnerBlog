b75b87d971ba778b1589121b379a4a174c74c76a
royweb <xytank@gmail.com>
2020-03-06 15:47:59 -0500

    Save the new post to the database

--- runnerblog/routes.py
+++ runnerblog/routes.py
@@ -153,6 +153,10 @@ def new_post():
     form = PostForm()
 
     if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        
         flash("Post Created", "success")
         return redirect(url_for("home"))
 
