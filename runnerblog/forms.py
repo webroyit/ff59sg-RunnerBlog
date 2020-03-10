4dc764178785827aa6dfc31b1a2275cafe35fcd0
royweb <xytank@gmail.com>
2020-03-10 13:51:57 -0400

    Create a form to request password change

--- runnerblog/forms.py
+++ runnerblog/forms.py
@@ -64,3 +64,13 @@ class PostForm(FlaskForm):
     title = StringField("Title", validators = [DataRequired()])
     content = TextAreaField("Content", validators = [DataRequired()])
     submit = SubmitField("Post")
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+
+        if user is None:
+            raise ValidationError("This email does not existed")
