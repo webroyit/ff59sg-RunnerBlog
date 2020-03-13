7e70768d87b3765f86fdbaef80bd11203f9f9f3f
royweb <xytank@gmail.com>
2020-03-13 14:54:56 -0400

    Remove some files

--- runnerblog/routes.py
+++
@@ -1,9 +0,0 @@
-import os
-import secrets
-from PIL import Image
-from flask import render_template, url_for, flash, redirect, request, abort
-from runnerblog import app, db, bcrypt, mail
-from runnerblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
-from runnerblog.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required
-from flask_mail import Message
