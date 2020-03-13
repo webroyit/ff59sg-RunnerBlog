0b45a0b9038833ce56aac921e06b0995d0c336fa
royweb <xytank@gmail.com>
2020-03-13 13:42:33 -0400

    Move the rest of the routes to the main folder

---
+++ runnerblog/main/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+
+main = Blueprint("main", __name__)
+
+# you can add more endpoints to use the same function
+@main.route("/")
+@main.route("/home")
+def home():
+    # type = int to return a error if value is not int type
+    page = request.args.get("page", 1, type = int)
+
+    # get some posts per page
+    # per_page for number of post for each page
+    # order_by() to sort the data
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
+    
+    return render_template("home.html", posts = posts)
+
+@main.route("/about")
+def about():
+    return render_template("about.html", title = "About")
--- runnerblog/routes.py
+++ runnerblog/routes.py
@@ -7,21 +7,3 @@ from runnerblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, Pos
 from runnerblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
-
-# you can add more endpoints to use the same function
-@app.route("/")
-@app.route("/home")
-def home():
-    # type = int to return a error if value is not int type
-    page = request.args.get("page", 1, type = int)
-
-    # get some posts per page
-    # per_page for number of post for each page
-    # order_by() to sort the data
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
-    
-    return render_template("home.html", posts = posts)
-
-@app.route("/about")
-def about():
-    return render_template("about.html", title = "About")
