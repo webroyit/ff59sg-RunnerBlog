429ad9c49b7d816b4ae222e365dcc0ae7928f03a
royweb <xytank@gmail.com>
2020-03-13 14:06:08 -0400

    Break up the classes on forms.py into sections

---
+++ runnerblog/users/forms.py
@@ -0,0 +1,69 @@
+class RegistrationForm(FlaskForm):
+    # DataRequired() = input cannot be empty
+    # Length() =  length of the input
+    # Email() = check if the input is an email
+    # EqualTo() = check if the password and confirm password match
+    username = StringField("Username", validators = [DataRequired(), Length(min = 3, max = 20)])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
+    confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password"), Length(min = 6, max = 20)])
+    submit = SubmitField("Create Account")
+
+    # custom validation
+    def validate_username(self, username):
+        # find the user in the database
+        user = User.query.filter_by(username = username.data).first()
+
+        if user:
+            # send a message
+            raise ValidationError("Username aleady existed")
+
+    def validate_email(self, email):
+        # find the email in the database
+        user = User.query.filter_by(email = email.data).first()
+
+        if user:
+            raise ValidationError("Email aleady existed")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Login")
+
+class UpdateAccountForm(FlaskForm):
+    # FileField is field for file type
+    # FileAllowed is a validation for file type
+    username = StringField("Username", validators = [DataRequired(), Length(min = 3, max = 20)])
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    picture = FileField("Update Profile Picture", validators = [FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+
+            if user:
+                raise ValidationError("Username aleady existed")
+
+    def validate_email(self, email):
+        if email.data != current_user.username:
+            user = User.query.filter_by(email = email.data).first()
+
+            if user:
+                raise ValidationError("Email aleady existed")
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators = [DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email = email.data).first()
+
+        if user is None:
+            raise ValidationError("This email does not existed")
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
+    confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password"), Length(min = 6, max = 20)])
+    submit = SubmitField("Reset Password")
