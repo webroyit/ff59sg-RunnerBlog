71d3df23f6f6a514ac9f292e3709a50f60a37ad2
royweb <xytank@gmail.com>
2020-03-14 13:44:53 -0400

    Add a function to initialize the extensions

--- runnerblog/__init__.py
+++ runnerblog/__init__.py
@@ -5,15 +5,9 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from runnerblog.config import Config
 
-# double underscore for the name of the module
-app = Flask(__name__)
-
-# use the variables from config.py
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 # redirect the user to the login page is they are not login
 login_manager.login_view = "users.login"
@@ -21,13 +15,28 @@ login_manager.login_view = "users.login"
 # change the color of the flash message
 login_manager.login_message_category = "info"
 
-mail = Mail(app)
+mail = Mail()
 
-# access to these routes
-from runnerblog.users.routes import users
-from runnerblog.posts.routes import posts
-from runnerblog.main.routes import main
+# initialize the extensions at the top of the file
+def create_app(config_class = Config):
+    # double underscore for the name of the module
+    app = Flask(__name__)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    # use the variables from config.py
+    app.config.from_object(Config)
+
+    # access to these routes
+    from runnerblog.users.routes import users
+    from runnerblog.posts.routes import posts
+    from runnerblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
